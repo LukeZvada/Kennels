f28abebe8738e8018d9d230a7b9de858983ea799
LukeZvada <zvadamusic@gmail.com>
2020-09-02 09:45:46 -0700

    first commit with all components rendering

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css";
+
+export const Customer = () => (
+    <section className="customer">
+        <h3 className="customer__Name">Hannah Hall</h3>
+        <div className="customer__Address">Address: 100 Infinity Way</div>
+    </section>
+)
---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css";
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__Name">Emma Beaton</h3>
+        <div className="employer__Name">Nashville Kennels North</div>
+    </section>
+)
