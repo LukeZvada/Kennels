03726c0185b5c602d69d6a084aa2a679f0bc9358
LukeZvada <zvadamusic@gmail.com>
2020-09-03 13:26:36 -0700

    rendering the nav bar and finding the owners and locations

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,27 +1,33 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider";
+import { LocationContext } from "../location/LocationProvider"
+import { CustomerContext } from "../customer/CustomerProvider"
 import "./Animal.css"
 
 
 export const AnimalList = () => {
     const {animals, getAnimals} = useContext(AnimalContext)
+    const {locations, getLocations} = useContext(LocationContext)
+    const {customers, getCustomers} = useContext(CustomerContext)
 
     useEffect(() => {
             getAnimals()
+            getLocations()
+            getCustomers()
         }, [])
-
-    // useEffect(() => {
-    // }, [animals])
-    //^this is not needed unless the animals array is being updated.
 
 
     return (
         <article className="animals">
             {
                 animals.map(animal => {
+                    const animalLocation = locations.find(location => (location.id === animal.locationId)) || {}
+                    const animalOwner = customers.find(customer => (customer.id === animal.customerId)) || {}
                     return <section key={animal.id} className="animal">
                         <div><h3>{animal.name}</h3></div>
                         <div>Breed: {animal.breed}</div>
+                        <div> Location: {animalLocation.name}</div>
+                        <div> Owner: {animalOwner.name}</div>
                     </section>
                 })
             }
